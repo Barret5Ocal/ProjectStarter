cb95812f8f8f24d8975a1ae301db620d2233e7cd
barret <asdf>
2019-08-23 15:15:39 -0700

    working

---
+++ code/filecontent.py
@@ -0,0 +1,160 @@
+CPPStarter = """ #include <windows.h>
+
+static int Running = 1; 
+
+#include <float.h>
+
+#include <stdint.h>
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef float real32;
+typedef double real64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float r32;
+typedef double r64;
+
+#define global_variable static 
+#define local_persist static 
+#define internal static 
+
+#define Assert(Expression) if(!(Expression)) {*(int *)0 = 0;}
+#define InvalidCodePath Assert(false)
+#define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
+
+#define Kilobyte(Value) 1024 * Value
+#define Megabyte(Value) 1024 * Kilobyte(Value)
+#define Gigabyte(Value) 1024 * Megabyte(Value)
+#define Terabyte(Value) 1024 * Gigabyte(Value)
+
+#define ScreenWidth 1280
+#define ScreenHeight 720
+
+struct win32_windowdim 
+{
+    int Width, Height; 
+    int x, y;
+    //int DisplayWidth, DisplayHeight; 
+};
+
+
+win32_windowdim Win32GetWindowDim(HWND Window)
+{
+    win32_windowdim Dim = {};
+    
+    RECT Rect = {};
+    //GetClientRect(Window, &Rect);
+    GetWindowRect(Window, &Rect);
+    Dim.x = Rect.left;
+    Dim.y = Rect.top;
+    Dim.Width = Rect.right - Rect.left;
+    Dim.Height = Rect.bottom - Rect.top;
+    return Dim; 
+}
+
+LRESULT CALLBACK
+MainWindowProc(HWND Window,
+               UINT Message,
+               WPARAM WParam,
+               LPARAM LParam)
+{
+    LRESULT Result = {};
+    
+    switch(Message)
+    {
+        case WM_DESTROY:
+        {
+            Running = 0;
+        }break;
+        case WM_CLOSE:
+        {
+            Running = 0;
+        }break;
+        case WM_KEYDOWN:
+        {
+          
+            
+        }break;
+        
+        case WM_LBUTTONDOWN: 
+        {
+            LeftMouse = 1;
+        }break;
+        case WM_LBUTTONUP: 
+        {
+            LeftMouse = 0;
+        }break;
+        default:
+        {
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        }break;
+    }
+    
+    return Result; 
+}
+
+
+int __stdcall WinMain(HINSTANCE Instance, 
+                      HINSTANCE PrevInstance,
+                      LPSTR CmdLine,
+                      int ShowCode)
+{
+    WNDCLASS WindowClass = {};
+    WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowProc;
+    WindowClass.hInstance = Instance;
+    WindowClass.lpszClassName = "NewProjectClass";
+    WindowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    
+    if(RegisterClass(&WindowClass))
+    {
+        HWND Window = CreateWindowEx(0, 
+                                     WindowClass.lpszClassName,
+                                     "New Project",
+                                     WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                                     CW_USEDEFAULT,
+                                     CW_USEDEFAULT,
+                                     ScreenWidth,
+                                     ScreenHeight,
+                                     0,
+                                     0,
+                                     Instance,
+                                     0);
+        
+
+        while(Running)
+        {
+            MSG Message;
+            while(PeekMessage(&Message, Window, 0, 0, PM_REMOVE))
+            {
+                TranslateMessage(&Message);
+                DispatchMessage(&Message);
+            }
+            
+
+        }
+    }
+    return 0; 
+}
+"""
+
+VSBat = """@echo off
+call \"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Enterprise\\VC\\Auxiliary\\Build\\vcvars64.bat\" 
+"""
---
+++ code/projectsetup.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+import filecontent
+
+path = os.getcwd()
+print ("The current working directory is %s" % path)
+
+projectFolder = '/' + sys.argv[1] 
+
+
+try:
+    os.mkdir(path + projectFolder)
+except OSError:
+    print ("Creation of the directory %s failed" % path)
+else:
+    print ("Successfully created the directory %s " % path)
+	
+try:
+    os.mkdir(path + projectFolder + "/project")
+except OSError:
+    print ("Creation of the directory %s failed" % path)
+else:
+    print ("Successfully created the directory %s " % path)
+	
+try:
+    os.mkdir(path + projectFolder + "/build")
+except OSError:
+    print ("Creation of the directory %s failed" % path)
+else:
+    print ("Successfully created the directory %s " % path)
+
+try:
+    os.mkdir(path + projectFolder + "/project" + "/code")
+except OSError:
+    print ("Creation of the directory %s failed" % path)
+else:
+    print ("Successfully created the directory %s " % path)
+	
+try:
+    os.mkdir(path + projectFolder + "/project" + "/data")
+except OSError:
+    print ("Creation of the directory %s failed" % path)
+else:
+    print ("Successfully created the directory %s " % path)
+	
+	
+	
+f = open(path + projectFolder + "/project" + "/code" + "/main.cpp","w+")
+
+f.write(filecontent.CPPStarter)
+
+f.close()
+
+
+f = open(path + projectFolder + "/shell.bat", "w+", encoding="utf-8")
+
+#filecontent.VSBat.encode("utf-8")
+
+f.write(filecontent.VSBat)
+
+f.close()
