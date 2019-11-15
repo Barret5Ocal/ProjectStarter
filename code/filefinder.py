34c35a28e284967bd84f4a86a010f922eaf840da
BARRET GAYLOR <BARGAYLO@uat.edu>
2019-11-15 13:10:52 -0700

    now threaded

--- code/filefinder.py
+++ code/filefinder.py
@@ -1,6 +1,6 @@
 import os
 import re
-#import win32api
+from multiprocessing import Process, Queue, Event
 
 def find_file(root_folder, rex):
     for root,dirs,files in os.walk(root_folder):
@@ -9,7 +9,18 @@ def find_file(root_folder, rex):
             if result:
                 return(os.path.join(root, f))
                 break # if you want to find only one
-
+                
+def find_file_multi(root_folder, rex, queue, quit, foundit):
+    for root,dirs,files in os.walk(root_folder):
+        if not quit.is_set():
+            for f in files:
+                result = rex.search(f)
+                if result:
+                    queue.put(os.path.join(root, f))
+                    foundit.set()
+                    break # if you want to find only one
+        else:
+            break
 """
 def find_file_in_all_drives(file_name):
     #create a regular expression for the file
@@ -22,3 +33,20 @@ def find_file_in_all_drives(file_name):
 def find_4ed():
     rex = re.compile('4ed\.exe' )
     return find_file('C:\\', rex)
+
+
+def find_4ed_fast():
+    rex = re.compile('4ed\.exe' )
+    dir_list = next(os.walk('C:\\'))[1]
+    jobs = []
+    queue = Queue()
+    quit = Event()
+    foundit = Event()
+    for dir in dir_list:
+        if dir != '$Recycle.Bin':
+            p = Process(target=find_file_multi, args=('C:\\'+dir+'\\',rex, queue, quit, foundit))   
+            jobs.append(p)
+            p.start()
+    foundit.wait()  
+    quit.set()   
+    return queue.get()
--- code/projectsetup.py
+++ code/projectsetup.py
@@ -4,72 +4,73 @@ import sys
 import filecontent
 import filefinder
 
-path = os.getcwd()
-print ("The current working directory is %s" % path)
+if __name__ == '__main__':
+    path = os.getcwd()
+    print ("The current working directory is %s" % path)
 
-projectFolder = '/' + sys.argv[1] 
-
-
-try:
-    os.mkdir(path + projectFolder)
-except OSError:
-    print ("Creation of the directory %s failed" % path)
-else:
-    print ("Successfully created the directory %s " % path)
-	
-try:
-    os.mkdir(path + projectFolder + "/project")
-except OSError:
-    print ("Creation of the directory %s failed" % path)
-else:
-    print ("Successfully created the directory %s " % path)
-	
-try:
-    os.mkdir(path + projectFolder + "/build")
-except OSError:
-    print ("Creation of the directory %s failed" % path)
-else:
-    print ("Successfully created the directory %s " % path)
-
-try:
-    os.mkdir(path + projectFolder + "/project" + "/code")
-except OSError:
-    print ("Creation of the directory %s failed" % path)
-else:
-    print ("Successfully created the directory %s " % path)
-	
-try:
-    os.mkdir(path + projectFolder + "/project" + "/data")
-except OSError:
-    print ("Creation of the directory %s failed" % path)
-else:
-    print ("Successfully created the directory %s " % path)
-	
-	
-	
-f = open(path + projectFolder + "/project" + "/code" + "/main.cpp","w+")
-
-f.write(filecontent.CPPStarter)
-
-f.close()
+    projectFolder = '/' + sys.argv[1] 
 
 
-f = open(path + projectFolder + "/shell.bat", "w+", encoding="utf-8")
+    try:
+        os.mkdir(path + projectFolder)
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+        
+    try:
+        os.mkdir(path + projectFolder + "/project")
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+        
+    try:
+        os.mkdir(path + projectFolder + "/build")
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
 
-#filecontent.VSBat.encode("utf-8")
+    try:
+        os.mkdir(path + projectFolder + "/project" + "/code")
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+        
+    try:
+        os.mkdir(path + projectFolder + "/project" + "/data")
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+        
+        
+        
+    f = open(path + projectFolder + "/project" + "/code" + "/main.cpp","w+")
 
-f.write(filecontent.VSBat)
+    f.write(filecontent.CPPStarter)
 
-f.close()
+    f.close()
 
 
-f = open(path + projectFolder + "/4ed.bat", "w+", encoding="utf-8")
+    f = open(path + projectFolder + "/shell.bat", "w+", encoding="utf-8")
 
-#filecontent.VSBat.encode("utf-8")
-path = filefinder.find_4ed()
+    #filecontent.VSBat.encode("utf-8")
 
-f.write("@echo off \nstart " + path + " -W")
+    f.write(filecontent.VSBat)
 
-f.close()
+    f.close()
+
+
+    f = open(path + projectFolder + "/4ed.bat", "w+", encoding="utf-8")
+
+    #filecontent.VSBat.encode("utf-8")
+    path = filefinder.find_4ed_fast()
+
+    f.write("@echo off \nstart " + path + " -W")
+
+    f.close()
 
 
