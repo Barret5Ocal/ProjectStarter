17d7c424fb16bfc908c66b0b3c54ece44b87843c
Barret5Ocal <barretgaylor@gmail.com>
2019-12-02 22:24:51 -0700

    more to shell

--- code/filecontent.py
+++ code/filecontent.py
@@ -164,6 +164,10 @@ IF EXIST C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\
 (
 call \"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\\Auxiliary\\Build\\vcvars64.bat\" 
 )
+IF EXIST C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
+(
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+)
 """
 
 
